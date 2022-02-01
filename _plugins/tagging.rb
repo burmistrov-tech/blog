96cbd1e8082249017e9706d25f540675c796f366
Nikita Burmistrov <nick.2001.60@gmail.com>
2022-02-01 10:21:05 +0100

    refactoring tagging

--- _plugins/tagging.rb
+++ _plugins/tagging.rb
@@ -3,28 +3,66 @@ module Jekyll
     safe true
 
     def generate(site)
-      tags = site.posts.docs.flat_map { |post| post.data['tags'] || [] }.to_set
-      tags.each do |tag|
-        site.pages << TagPage.new(site, site.source, tag)
+      tags_posts = TagsPostsCollector.new(site).collect()
+
+      site.pages << TagListPage.new(site, site.source, tags_posts)
+
+      tags_posts.each_pair do |tag, posts|
+        site.pages << TagPage.new(site, site.source, tag, posts)
       end
+    end
+    
+    private
+
+    class TagsPostsCollector
+      def initialize(site)
+        @site = site
+      end
+  
+      def collect()
+        tags_posts = Hash.new
+        
+        @site.posts.docs.each do |post|
+          post.data['tags'].each do |tag|
+            if tags_posts[tag]
+              tags_posts[tag] << post            
+            else          
+              tags_posts[tag] = [ post ]
+            end
+          end
+        end
+  
+        return tags_posts
+      end
+    end      
+  end
+
+  class TagListPage < Page
+    def initialize(site, base, tags_posts)
+      @site = site
+      @base = base
+      @dir  = File.join('tags')
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag-list.html')
+      self.data['tags_posts'] = tags_posts
+      self.data['tags'] = tags_posts.keys
+      self.data['posts'] = tags_posts.values
     end
   end
 
   class TagPage < Page
-    def initialize(site, base, tag)
+    def initialize(site, base, tag, posts)
       @site = site
       @base = base
       @dir  = File.join('tags', tag)
       @name = 'index.html'
-
-      posts = site.posts.docs.select { |post| post.data['tags'].include?(tag) }
-      posts_count = posts.count()
       
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag.html')
       self.data['tag'] = tag
       self.data['posts'] = posts
-      self.data['title'] = posts_count > 1 ? "There are #{posts_count} posts about #{tag}" : "There is #{posts_count} post about #{tag}"
     end
   end
 end
